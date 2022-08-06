898dc10cd6fec410d6f55a65e7be6f684a8b0669
murilo-simoes <murilorsimoes@gmail.com>
2022-08-06 09:31:01 -0300

    fix: input

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ function ChecarGrau() {
     if (esferico_direito == 0 && esferico_esquerdo == 0 && cilindrico_direito == 0 && cilindrico_esquerdo == 0) {
         lente.innerHTML = "<p>Preencha algum campo</p>"
     } else {
-        if ((cilindrico_direito <= 0 || cilindrico_esquerdo <= 0) && (cilindrico_direito >= -5 && cilindrico_esquerdo >= -5) ) {
+        if ((cilindrico_direito <= 0 || cilindrico_esquerdo <= 0) && (cilindrico_direito >= -5 && cilindrico_esquerdo >= -5)) {
             if ((esferico_esquerdo <= 0 && esferico_esquerdo >= -15) && (esferico_direito <= 0 && esferico_direito >= -15)) {
                 lente.innerHTML = "<p>Lente Vision</p>"
             }
         }
-        
+
         if ((cilindrico_direito == 0 && cilindrico_esquerdo == 0) && (cilindrico_direito >= -2 && cilindrico_esquerdo >= -2)) {
             if ((esferico_esquerdo <= -3 && esferico_esquerdo >= -12) && (esferico_direito <= -3 && esferico_direito >= -12)) {
                 lente.innerHTML = "<p>Lente Prime</p>"
             }
         }
-        
+
         if ((cilindrico_direito < 0 || cilindrico_esquerdo < 0) && (cilindrico_direito >= -2 && cilindrico_esquerdo >= -2)) {
             if ((esferico_esquerdo <= -3 && esferico_esquerdo >= -10) && (esferico_direito <= -3 && esferico_direito >= -10)) {
                 lente.innerHTML = "<p>Lente Prime</p>"
             }
         }
-        
-        if(cilindrico_direito < -5 || cilindrico_esquerdo < -5){
+
+        if (cilindrico_direito < -5 || cilindrico_esquerdo < -5) {
             lente.innerHTML = "<p>Não há lente com essas informações!</p>"
         }
     }
